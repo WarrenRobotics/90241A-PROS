c89ec66a81c7e00a3611e3bd8de41b5699d5d0ab
Berger Engle <717572@student.dusd.net>
2019-02-27 10:52:43 -0800

    Adding separate ports header file

---
+++ include/ports.h
@@ -0,0 +1,19 @@
+
+/*
+  file ports.h
+
+  This file is a current work in process. It is used to define the ports of
+  each device plugged into the V5 brain. 
+*/
+#ifndef  _PROS_PORTS_H
+#define _PROS_PORTS_H
+
+#define DRIVE_LEFT_FRONT_PORT 10
+#define DRIVE_LEFT_BACK_PORT 2
+#define DRIVE_RIGHT_FRONT_PORT 4
+#define DRIVE_RIGHT_BACK_PORT 1
+#define FLYWHEEL_LOWER_PORT 19
+#define FLYWHEEL_UPPER_PORT 11
+#define INTAKE_PORT 3
+
+#endif
--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,12 +1,5 @@
 #include "main.h"
 
-#define DRIVE_LEFT_FRONT_PORT 10
-#define DRIVE_LEFT_BACK_PORT 2
-#define DRIVE_RIGHT_FRONT_PORT 4
-#define DRIVE_RIGHT_BACK_PORT 1
-#define FLYWHEEL_LOWER_PORT 19
-#define FLYWHEEL_UPPER_PORT 11
-#define INTAKE_PORT 3
 
 /**
  * Runs the operator control code. This function will be started in its own task
